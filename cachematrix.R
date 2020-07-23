3199e5ac6eaf5fd04bcc81e96a308c1d854684eb
Mystic Grey <stephinrock@gmail.com>
2020-07-23 20:08:33 +0530

    A R code for obtaining the inverse of a matrix and caching the result using Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+        Cache_Inverse <- NULL
+        set <- function(y) {
+                x <<- y
+                Cache_Inverse <<- NULL
+        }
+        get <- function() x
+        Set_Inverse <- function(Inverse_Matrix) Cache_Inverse <<- Inverse_Matrix
+        Get_Inverse <- function() Cache_Inverse
+        list(set = set, get = get,
+             Set_Inverse = Set_Inverse,
+             Get_Inverse = Get_Inverse)
 }
 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        Inverse_Function <- x$Get_Inverse()
+        if(!is.null(Inverse_Function)) {
+                message("getting cached data")
+                return(Inverse_Function)
+        }
+        Input_Matrix <- x$get()
+        Inverse_Function <- solve(Input_Matrix, ...)
+        x$Set_Inverse(Inverse_Function)
+        Inverse_Function
 }
